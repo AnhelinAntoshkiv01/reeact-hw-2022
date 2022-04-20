841c9f3c04a5a86b4d1024a4d0248d5b5c6d6dab
AnhelinAntoshkiv01 <antoshkivangel@gmail.com>
2022-04-20 14:27:39 +0300

    hw - 5.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import {useReducer, useState} from "react";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const reducer = (state, action) => {
+  const {type, payload} = action;
+
+  switch (type) {
+      case 'addCat':
+          return { ...state, cats: [...state.cats, {name: payload, id: Date.now()}]}
+      case 'deleteCat':
+          return { ...state, cats: state.cats.filter(cat => cat.id !== payload)}
+
+    case 'addDog':
+      return { ...state, dogs: [...state.dogs, {name: payload, id: Date.now()}]}
+    case 'deleteDog':
+      return { ...state, dogs: state.dogs.filter(dog => dog.id !== payload)}
+
+    default:
+      console.error('Unknown type!');
+      return state;
+  }
 }
 
-export default App;
+
+export const App = () => {
+  const [state, dispatch] = useReducer( reducer, {cats: [], dogs: []} );
+
+  const [catValue, setCatValue] = useState('');
+  const [dogValue, setDogValue] = useState('');
+
+  const createCat = () => {
+    dispatch({ type: 'addCat', payload: catValue });
+    setCatValue('');
+  }
+
+  const createDog = () => {
+      dispatch({ type: 'addDog', payload: dogValue });
+      setDogValue('');
+  }
+
+  return (
+      <div>
+        <div className={'header'}>
+          <div>
+              <label>Cat name: <input type="text" onChange={({target}) => setCatValue(target.value)} value={catValue}/></label>
+              <button onClick={createCat}>Save</button>
+          </div>
+          <div>
+              <label>Dog name: <input type="text" onChange={({target}) => setDogValue(target.value)} value={dogValue}/></label>
+              <button onClick={createDog}>Save</button>
+          </div>
+        </div>
+
+        <hr/>
+
+          <div className={'header'}>
+              <div>
+                  {
+                      state.cats.map(cat => (
+                          <div key={cat.id}>
+                              {cat.name}
+                              <button onClick={() => dispatch({type: 'deleteCat', payload: cat.id})}>Delete</button>
+                          </div>
+                      ))
+                  }
+              </div>
+              <div>
+                  {
+                      state.dogs.map(dog => (
+                          <div key={dog.id}>
+                              {dog.name}
+                              <button onClick={() => dispatch({type: 'deleteDog', payload: dog.id})}>Delete</button>
+                          </div>
+                      ))
+                  }
+              </div>
+          </div>
+      </div>
+  );
+}
